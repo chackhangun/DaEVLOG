76e9d45cdcf2269d61900d1186cc84c41a61c2e8
Eric Choi <chackhangun@gmail.com>
2024-03-19 22:03:19 +0900

    test

--- src/component/Logo.tsx
+++ src/component/Logo.tsx
@@ -2,32 +2,38 @@ import styled from "styled-components";
 
 export const Logo = () => (
   <LogoWrapper>
-    <LOGOTEXT_D>D</LOGOTEXT_D>
-    <LOGOTEXT_EVLOG>EVLOG</LOGOTEXT_EVLOG>
+    <Wrapper2></Wrapper2>
   </LogoWrapper>
 );
 
 const LogoWrapper = styled.div`
   width: 100%;
   display: flex;
+  flex-direction: column;
+`;
+
+const Wrapper1 = styled.div`
+  font-size: 40px;
+  font-weight: bold;
+  color: #ffffff;
+  display: flex;
   justify-content: center;
+  display: flex;
+  justify-content: center;
+  font-family: Mr Dafoe, sans-serif;
+  text-align: center;
+  text-shadow: 0 -3px 0 #333, 0 6px 8px rgba(255, 255, 255, 0.4),
+    0 9px 10px rgba(255, 255, 255, 0.15);
+`;
+
+const Wrapper2 = styled.div`
   margin-bottom: 24px;
-`;
-
-const LOGOTEXT_D = styled.div`
-  font-size: 40px;
+  font-size: 26px;
   font-weight: bold;
   color: #ffffff;
-
-  &::after {
-    content: "a";
-    font-size: 14px;
-    color: #f7b500;
-  }
-`;
-
-const LOGOTEXT_EVLOG = styled.div`
-  font-size: 40px;
-  font-weight: bold;
-  color: #ffffff;
+  justify-content: center;
+  display: flex;
+  text-align: center;
+  text-shadow: 0 -3px 0 #333, 0 6px 8px rgba(255, 255, 255, 0.4),
+    0 9px 10px rgba(255, 255, 255, 0.15);
 `;
